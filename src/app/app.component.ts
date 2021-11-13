bca90b2218ed21a2cf2f2efec0043cdd87a2dcfa
ibrahimbanat <Ibrahim.banat@altibbi.com>
2021-11-13 20:22:05 +0200

    finish working on directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,104 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives';
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60'
+    },
+    {
+      title: 'Girl setting',
+      url:
+      'https://images.unsplash.com/photo-1636731990905-4d086f23a73a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=734&q=80'
+    },
+    {
+      title: 'Highway',
+      url:
+      'https://images.unsplash.com/photo-1636640157431-8db7c0df9480?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      title: 'At station',
+      url: 'https://images.unsplash.com/photo-1636734679358-2810287f6c1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80'
+    },
+    {
+      title: 'Targaryen castle',
+      url: 'https://images.unsplash.com/photo-1636725691774-e9c7cfb150ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60'
+    },
+    {
+      title: 'Girl setting',
+      url:
+      'https://images.unsplash.com/photo-1636731990905-4d086f23a73a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=734&q=80'
+    },
+    {
+      title: 'Highway',
+      url:
+      'https://images.unsplash.com/photo-1636640157431-8db7c0df9480?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      title: 'At station',
+      url: 'https://images.unsplash.com/photo-1636734679358-2810287f6c1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80'
+    },
+    {
+      title: 'Targaryen castle',
+      url: 'https://images.unsplash.com/photo-1636725691774-e9c7cfb150ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60'
+    },
+    {
+      title: 'Girl setting',
+      url:
+      'https://images.unsplash.com/photo-1636731990905-4d086f23a73a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=734&q=80'
+    },
+    {
+      title: 'Highway',
+      url:
+      'https://images.unsplash.com/photo-1636640157431-8db7c0df9480?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      title: 'At station',
+      url: 'https://images.unsplash.com/photo-1636734679358-2810287f6c1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80'
+    },
+    {
+      title: 'Targaryen castle',
+      url: 'https://images.unsplash.com/photo-1636725691774-e9c7cfb150ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60'
+    },
+    {
+      title: 'Girl setting',
+      url:
+      'https://images.unsplash.com/photo-1636731990905-4d086f23a73a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=734&q=80'
+    },
+    {
+      title: 'Highway',
+      url:
+      'https://images.unsplash.com/photo-1636640157431-8db7c0df9480?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      title: 'At station',
+      url: 'https://images.unsplash.com/photo-1636734679358-2810287f6c1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80'
+    },
+    {
+      title: 'Targaryen castle',
+      url: 'https://images.unsplash.com/photo-1636725691774-e9c7cfb150ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
+    },
+
+  ];
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClassDirective } from './class.directive';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassDirective,
+    TimesDirective
   ],
   imports: [
     BrowserModule
